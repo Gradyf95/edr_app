2a1b22621ac8795aad5d2a7c1bbdc1d49f158b41
gradyf95 <gradyf95@gmail.com>
2023-12-09 16:02:55 -0700

    basic process controller done. Will check Monday to see if it needs to be more/different

--- app/models/process_controller.rb
+++ app/models/process_controller.rb
@@ -2,6 +2,10 @@ class ProcessController < BaseModel
 
   # Takes in a prompt and returns a ProcessActivity object
   def self.start_process(prompt)
-
+    time = Time.now
+    io = IO.popen('ruby test.rb')
+    $stdout << io.read
+    
+    ProcessActivity.new(time: time)
   end
 end
--- spec/models/process_controller_spec.rb
+++ spec/models/process_controller_spec.rb
@@ -1,5 +1,23 @@
 require './app/models/process_controller.rb'
+require './app/models/process_activity.rb'
 
 describe ProcessController do
-  
+  let(:time) { Time.now }
+  let(:prompt) { double('prompt') }
+  let(:io) { double('io') }
+
+  before do
+    allow(IO).to receive(:popen).with('ruby test.rb').and_return(io)
+    allow(io).to receive(:read).and_return('hello world')
+    # this stops hello world from being output in our rspec output
+    allow($stdout).to receive(:<<)
+  end
+
+  it 'outputs to stdout' do
+    expect{ described_class.start_process(prompt) }.to output(a_string_including('hello world')).to_stdout
+  end
+
+  it 'returns a ProcessActivity object' do
+    expect(described_class.start_process(prompt)).to be_a ProcessActivity
+  end
 end
